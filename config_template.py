e98ebc7272886482f1035f729cc8a1ccc9abc155
Christopher Cowin <christophercowin@Christophers-MacBook-Pro.local>
2019-01-27 12:39:18 -0500

    error handling

---
+++ config_template.py
@@ -0,0 +1,2 @@
+smart_things_token=""
+device_ids = [""]  # only one device to turn off supported for now
--- hdmi_cec_monitor.py
+++ hdmi_cec_monitor.py
@@ -3,25 +3,30 @@ import aiohttp
 import pysmartthings
 import asyncio
 import cec
+import logging
+import config
 
-token = "SMART THINGS TOKEN HERE"
+smart_things_token = config.smart_things_token
 cec.init()
 tv_state = False
-s = sched.scheduler(time.time, time.sleep)
+schedule = sched.scheduler(time.time, time.sleep)
 
 
 async def turn_off_light():
     async with aiohttp.ClientSession() as session:
-        api = pysmartthings.SmartThings(session, token)
-        devices = await api.devices(device_ids=["DEVICE TO TURN OFF ID"])
-        light = devices[0]
-        result = await light.command("switch", "off")
-        assert result == True
+        try:
+            api = pysmartthings.SmartThings(session, smart_things_token)
+            devices = await api.devices(device_ids=config.device_ids)
+            light = devices[0]
+            result = await light.command("switch", "off")
+            assert result
+        except Exception as err:
+            logging.warning("Unable to toggle light: " + err)
 
 
 def get_tv_state():
     # devices = cec.list_devices()
-    tv = cec.Device(0) #Assume TV is first device
+    tv = cec.Device(0)  # Assume TV is first device
     return tv.is_on()
 
 
@@ -35,10 +40,14 @@ def check_state():
         if new_tv_state:
             loop.run_until_complete(turn_off_light())
 
-    s.enter(5, 1, check_state)
+    schedule.enter(5, 1, check_state)
 
 
 loop = asyncio.get_event_loop()
-check_state()
-s.run()
-loop.close()
+try:
+    check_state()
+except Exception as err:
+    logging.warning("Failure checking state of CEC: " + err)
+finally:
+    schedule.run()
+    loop.close()
